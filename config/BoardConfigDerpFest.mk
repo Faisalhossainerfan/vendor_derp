4cb0d26f7ab7257f9ff84de29f61540329adf3c2
Julian Veit <claymore1298@gmail.com>
2020-04-08 23:29:56 +0200

    derp: improve TARGET_KERNEL_CLANG_CUSTOM Boardflag
    When Boardflag TARGET_KERNEL_CLANG_VERSION is just set to "latest,
    the build process will choose automatic the latest clang version.
    The version to choose is defined in custom-clang repo in clang_custom.mk
    in variable CLANG_CUSTOM_VERSION_LATEST.
    
    Change-Id: I6e5a80d5480a41526151828c8651fa18f61e00b6
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/BoardConfigDerpFest.mk
+++ config/BoardConfigDerpFest.mk
@@ -1,5 +1,7 @@
+ifeq ($(TARGET_KERNEL_CLANG_VERSION),latest)
+include prebuilts/clang/host/linux-x86_custom/clang_custom.mk
+endif
 include vendor/derp/config/BoardConfigKernel.mk
-
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/derp/config/BoardConfigQcom.mk
 endif
