7a7def44be75ccff323ad7c0170f781803330915
George Zacharia <george.zcharia@gmail.com>
2021-01-20 08:04:27 +0100

    kernel: Cleanup some of the clang logic
    Only handle the AOSP Clang logic here, other variants will have to depend on the variable available
    
    Change-Id: I3281eca9720a1c498544e6d9b18581ce0b949d4f
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/BoardConfigDerpFest.mk
+++ config/BoardConfigDerpFest.mk
@@ -1,7 +1,7 @@
+include vendor/derp/config/BoardConfigKernel.mk
 ifeq ($(TARGET_KERNEL_CLANG_VERSION),latest)
 include prebuilts/clang/host/linux-x86_custom/clang_custom.mk
 endif
-include vendor/derp/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/derp/config/BoardConfigQcom.mk
 endif
